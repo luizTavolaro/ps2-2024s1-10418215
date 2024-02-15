7247f9034fbc82be3c8f35e308a68c362b6acb03
luizTavolaro <120058711+luizTavolaro@users.noreply.github.com>
2024-02-15 20:09:08 -0300

    initial commit

---
+++ semana-02/q19.java
@@ -0,0 +1,14 @@
+package exjv2;
+
+public class q19 {
+	public static void main(String [] args) {
+		String [] regioes = {"N","NE","CO","SE","S"};
+		int n = regioes.length;
+		for (int i = 0; i < n; i++) {
+			System.out.print(regioes[i]);
+			System.out.print(" ");
+		}
+	}
+}
+
+
