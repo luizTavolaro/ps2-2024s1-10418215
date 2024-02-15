e8749ad99b42ff9e9b268d6310d247129bbb5f20
luizTavolaro <120058711+luizTavolaro@users.noreply.github.com>
2024-02-15 23:14:50 +0000

    feat: q20

---
+++ semana-02/q20.java
@@ -0,0 +1,14 @@
+package exjv2;
+
+public class q20 {
+	public static void main(String [] args) {
+		String [] regioes = {"N","NE","CO","SE","S"};
+		int n = regioes.length;
+		for (int i = 4; i > -1; i--) {
+			System.out.print(regioes[i]);
+			System.out.print(" ");
+		}
+	}
+}
+
+
