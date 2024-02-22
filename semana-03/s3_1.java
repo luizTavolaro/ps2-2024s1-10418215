7762ee7b3a3ed652408a886680dd861978c714bd
luizTavolaro <120058711+luizTavolaro@users.noreply.github.com>
2024-02-22 20:48:15 -0300

    Update s3_1.java

--- semana-03/s3_1.java
+++ semana-03/s3_1.java
@@ -14,6 +14,9 @@ public class s3_1 {
 			String nome = s3_1_filmes.consultar(linha);
 			showMessageDialog(null,nome);
 		}
+		catch(s3_exception e) {
+			showMessageDialog(null,"Posição não existe");
+		}
 		catch(Exception e) {
 			showMessageDialog(null,"Filme não encontrado");
 		}
@@ -21,11 +24,13 @@ public class s3_1 {
 }
 
 class s3_1_filmes{
-	public static String consultar(int indice) throws Exception {
+	public static String consultar(int indice) throws Exception, s3_exception {
 		Path arquivo = Paths.get("filmes.txt");
 		String nomesFilmes [];
 		nomesFilmes = Files.readAllLines(arquivo).toArray(new String[0]);
+		if(indice > nomesFilmes.length) {
+			throw new s3_exception();
+		}
 		return nomesFilmes[indice];
-		
 	}
 }
